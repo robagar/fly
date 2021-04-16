e5783e1989a65f5ddb76b5e1cc10794857918881
Rob Agar <robagar@fastmail.net>
2021-04-16 10:16:25 +0100

    test with output working

--- test_with_output.py
+++ test_with_output.py
@@ -5,15 +5,15 @@ from djitellopy import Tello
 tello = Tello()
 
 def output_state():
-    try:
-        h = tello.get_height()
-        print(f'height: {h}')
-    except:
-        pass
+    s = tello.get_current_state()
+    print('state:')
+    for k,v in s.items():
+        print(f'  {k}: {v}')
 
 def poll_and_output_state():
-    output_state()    
-    sleep(1)
+    while True:
+        output_state()    
+        sleep(1)
 
 output_state_thread = Thread(target=poll_and_output_state, daemon=True)
 output_state_thread.start()
@@ -21,8 +21,8 @@ output_state_thread.start()
 tello.connect()
 tello.takeoff()
 
-#tello.move_left(100)
-#tello.rotate_counter_clockwise(90)
-tello.move_forward(50)
+for i in range(4):
+    tello.move_forward(50)
+    tello.rotate_counter_clockwise(90)
 
 tello.land()
