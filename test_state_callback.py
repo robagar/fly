46ea7c698278c4f93e13143beffe9034fd3bcb7f
Rob Agar <robagar@fastmail.net>
2021-04-16 10:45:26 +0100

    test state callback

---
+++ test_state_callback.py
@@ -0,0 +1,21 @@
+from threading import Thread
+from time import sleep
+from djitellopy import Tello
+
+tello = Tello()
+
+def on_state_received(drone_address, state):
+    print(f'state received from {drone_address}')
+    for k,v in state.items():
+        print(f'  {k}: {v}')
+
+Tello.on_state_received = on_state_received
+
+tello.connect()
+tello.takeoff()
+
+for i in range(4):
+    tello.move_forward(50)
+    tello.rotate_counter_clockwise(90)
+
+tello.land()
